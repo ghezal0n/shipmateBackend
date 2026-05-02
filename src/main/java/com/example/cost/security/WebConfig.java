c3e612a033184d81eb8679c43771ef516f43cb51
Mohamed Mehdi Ghezal <mohamedmehdighezal@192.168.1.100>
2026-05-02 20:53:48 +0100

    fix cors vercel

--- src/main/java/com/example/cost/security/WebConfig.java
+++ src/main/java/com/example/cost/security/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.cost.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
@@ -9,10 +8,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // permet toutes les routes
-                .allowedOrigins("http://localhost:3000") // autorise le frontend
-                .allowedMethods("*") // GET, POST, PUT, DELETE...
-                .allowedHeaders("*") // autorise tous les headers
-                .allowCredentials(true); // permet l’envoi du token
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://shipmate-alpha.vercel.app"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
